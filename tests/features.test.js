1606d583c0df917eaff952f035ecd9c6a01c7eb4
mike <cspkno005@gmail.com>
2026-04-16 07:05:30 +0800

    test: 💍 remove tests

--- tests/features.test.js
+++ tests/features.test.js
@@ -10,21 +10,25 @@ test.beforeEach(async ({ page }) => {
   });
 });
 
-test.describe('mouse scroll testing', () => {
-  test('F5 stop function test', async ({ page }) => {
-    await page.goto('http://localhost:5173/#/');
-    await page.getByRole('figure', { name: '對盔甲施法的卷軸 能增加防具的防禦力' }).getByRole('img').click();
-    await page.locator('img').first().click();
-    await page.locator('li:nth-child(9)').click();
-    await expect(page.locator('[id="🏠pageGame__X2"]')).toContainText('請選擇一種防具。');
+test.describe('scroll feature test', () => {
+  test('weapon to +1', async ({ page }) => {
+    await page.goto('http://localhost:5173/#/pageGame');
+    await expect(page.getByRole('figure', { name: '對武器施法的卷軸 能增加武器的攻擊力' }).getByRole('img')).toBeVisible();
+    await page.getByRole('figure', { name: '對武器施法的卷軸 能增加武器的攻擊力' }).getByRole('img').click();
+    await expect(page.locator('[id="🧱UIChat"]')).toContainText('請選擇一種武器。');
+    await expect(page.locator('[id="🧱StatusEquips__Equip"]').first()).toBeVisible();
+    await page.locator('[id="🧱StatusEquips__Equip"]').first().click();
+    await expect(page.locator('[id="🧱UIChat"]')).toContainText('+0 瑟魯基之劍 一瞬間發出 藍色的 光芒。');
   });
 
-  test('F9 stop function test', async ({ page }) => {
-    await page.goto('http://localhost:5173/#/');
-    await page.getByRole('figure', { name: '對盔甲施法的卷軸 能增加防具的防禦力' }).getByRole('img').click();
-    await page.locator('img').first().click();
-    await page.locator('li:nth-child(10)').click();
-    await expect(page.locator('[id="🏠pageGame__X2"]')).toContainText('請選擇一種防具。');
+  test('weapon to -1', async ({ page }) => {
+    await page.goto('http://localhost:5173/#/pageGame');
+    await expect(page.locator('.F12 > img')).toBeVisible();
+    await page.locator('.F12 > img').click();
+    await expect(page.locator('[id="🧱UIChat"]')).toContainText('請選擇一種武器。');
+    await expect(page.locator('[id="🧱StatusEquips__Equip"]').first()).toBeVisible();
+    await page.locator('[id="🧱StatusEquips__Equip"]').first().click();
+    await expect(page.locator('[id="🧱UIChat"]')).toContainText('+0 瑟魯基之劍 一瞬間發出 黑色的 光芒。');
   });
 
   test('armor to +1', async ({ page }) => {
@@ -45,25 +49,5 @@ test.describe('mouse scroll testing', () => {
     await page.locator('.armor.gloves').click();
     await expect(page.locator('[id="🧱UIChat"]')).toContainText('+0 力量手套 一瞬間發出 黑色的 光芒。');
     await expect(page.locator('[id="🧱UINumbers__X2__Ac"]')).toContainText('-6');
-  });
-
-  test('weapon to +1', async ({ page }) => {
-    await page.goto('http://localhost:5173/#/pageGame');
-    await expect(page.getByRole('figure', { name: '對武器施法的卷軸 能增加武器的攻擊力' }).getByRole('img')).toBeVisible();
-    await page.getByRole('figure', { name: '對武器施法的卷軸 能增加武器的攻擊力' }).getByRole('img').click();
-    await expect(page.locator('[id="🧱UIChat"]')).toContainText('請選擇一種武器。');
-    await expect(page.locator('[id="🧱StatusEquips__Equip"]').first()).toBeVisible();
-    await page.locator('[id="🧱StatusEquips__Equip"]').first().click();
-    await expect(page.locator('[id="🧱UIChat"]')).toContainText('+0 瑟魯基之劍 一瞬間發出 藍色的 光芒。');
-  });
-
-  test('weapon to -1', async ({ page }) => {
-    await page.goto('http://localhost:5173/#/pageGame');
-    await expect(page.locator('.F12 > img')).toBeVisible();
-    await page.locator('.F12 > img').click();
-    await expect(page.locator('[id="🧱UIChat"]')).toContainText('請選擇一種武器。');
-    await expect(page.locator('[id="🧱StatusEquips__Equip"]').first()).toBeVisible();
-    await page.locator('[id="🧱StatusEquips__Equip"]').first().click();
-    await expect(page.locator('[id="🧱UIChat"]')).toContainText('+0 瑟魯基之劍 一瞬間發出 黑色的 光芒。');
   });
 })
